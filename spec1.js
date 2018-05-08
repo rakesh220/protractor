e7ac489a7e17305a59816bfa1e60078bdef6eae7
rakesh220 <rakesh.kondabala66@gmail.com>
2018-05-08 20:27:57 +0530

    changed

--- spec1.js
+++ spec1.js
@@ -1,11 +1,12 @@
 describe('User should be able to login into application with credentials', function() {
-  
+  //login test cases
   it('login with correct userid and password', function() {
     browser.get('https://chatapp.aadhya-analytics.com:4201');
   element(by.id("Username")).sendKeys("rakesh");
   element(by.id("Password")).sendKeys("rakesh228");
   element(by.buttonText("Sign In")).click();
- browser.driver.sleep(40000);  
+ browser.driver.sleep(40000); 
+ 
   });
  
 });
